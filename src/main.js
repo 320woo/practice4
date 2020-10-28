e2130e3841d4723f91260b09a38ddedf44894859
hyunwoo-sl <tittyd@naver.com>
2020-10-28 15:55:22 +0900

    2020/10/27

--- src/main.js
+++ src/main.js
@@ -4,17 +4,15 @@ import store from './store'
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import { XyzSource, Map, TileLayer, OsmSource, Geoloc } from 'vuelayers'
+import 'vuelayers/lib/style.css'
 
-Vue.use(Vuetify, {
-  iconfont: 'md',
-  theme: {
-    primary: '#3f51b5',
-    secondary: '#b0bec5',
-    accent: '#8c9eff',
-    error: '#b71c1c'
-  }
-})
-
+Vue.use(XyzSource)
+Vue.use(Vuetify)
+Vue.use(Map)
+Vue.use(TileLayer)
+Vue.use(OsmSource)
+Vue.use(Geoloc)
 Vue.config.productionTip = false
 
 new Vue({
